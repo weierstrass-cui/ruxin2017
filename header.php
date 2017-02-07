8474b4166d6c9977a068ed8c20b6ed698aa737b4
weierstrass-cui <244169731@qq.com>
2017-02-07 20:26:17 +0800

    样式微调，案例页默认加载id变更

--- header.php
+++ header.php
@@ -162,7 +162,7 @@
 	            <a href="<?php bloginfo('home'); ?>/service/">服务体系</a>
 	          </li>
 	          <li<?php if( is_page(6) ) echo ' class="active"'; ?>>
-	            <a href="<?php bloginfo('home'); ?>/gallery?cat=12">成功案例</a>
+	            <a href="<?php bloginfo('home'); ?>/gallery?cat=6">成功案例</a>
 	          </li>
 	          <li<?php if( is_page(8) ) echo ' class="active"'; ?>>
 	            <a href="<?php bloginfo('home'); ?>/about/">关于我们</a>
