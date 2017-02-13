131ee39b44344be45d7775a1fd264fe823d9b5f3
weierstrass-cui <244169731@qq.com>
2017-02-13 14:00:51 +0800

    手机版案例分类

--- template_gallery.php
+++ template_gallery.php
@@ -174,10 +174,14 @@ get_header(); ?>
         </script>
     </div>
     <div class="anliList moShow">
-        <?php $posts = get_posts("category=4&numberposts=4"); ?>
+        <?php if( $_GET['mocat'] ){
+            $posts = get_posts("category=".$_GET['mocat']."&numberposts=99");
+        }else{
+            $posts = get_posts("category=4&numberposts=4");
+        } ?>
         <?php if( $posts ):?>
           <?php foreach( $posts as $index => $post ): setup_postdata( $post ); ?>
-              <div class="oneAnli" style="background:url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large')[0]; ?>) no-repeat;background-size:100% 100%;">
+            <div class="oneAnli" style="background:url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large')[0]; ?>) no-repeat;background-size:100% 100%;">
                 <div class="anliMask">
                     <div class="shotTitle"><?php echo get_post_meta($post->ID,'短标题',true); ?></div>
                     <h3><?php the_title(); ?></h3>
@@ -187,5 +191,29 @@ get_header(); ?>
             </div>
           <?php endforeach; ?>
         <?php endif ?>
+        <a href="javascript://" class="mo-changeCategory"></a>
+        <div class="mo-categroy-list">
+            <ul>
+                <li><a href="<?php bloginfo('home'); ?>/gallery">最新</a></li>
+                <?php  $categorys = getchild(4); ?>
+                <?php if( $categorys ): ?>
+                  <?php foreach( $categorys as $index => $item ): ?>
+                    <li>
+                        <a href="<?php bloginfo('home'); ?>/gallery?<?php echo 'mocat='.$item->term_id; ?>"><?php echo $item->cat_name ?></a>
+                    </li>
+                  <?php endforeach; ?>
+                <?php endif ?>
+            </ul>
+        </div>
+        <script type="text/javascript">
+            $(function(){
+                $('.mo-changeCategory').click(function(){
+                    $('.mo-categroy-list').toggle();
+                });
+                $('.mo-categroy-list').click(function(){
+                    $('.mo-categroy-list').hide();
+                });
+            });
+        </script>
     </div>
 <?php get_footer(); ?>
