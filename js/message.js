5c03846a831338e584f7e272a3d3766665d3347f
weierstrass-cui <244169731@qq.com>
2017-02-13 09:41:26 +0800

    留言功能

--- js/message.js
+++ js/message.js
@@ -4,8 +4,9 @@ $(function(){
         //Get the data from all the fields
         var author = $('input[name=author]');
         var phone = $('input[name=phone]');
-        // var website = $('input[name=website]');
+        var company = $('input[name=company]');
         var message = $('textarea[name=message]');
+
         var comment_post_ID = $('input[name=comment_post_ID]');
 		var comment_parent = $('input[name=comment_parent]');
         var returnError = false;
@@ -23,10 +24,10 @@ $(function(){
             returnError = true;
         } else phone.removeClass('error');
 
-        if (message.val()=='') {
-            message.addClass('error');
+        if (company.val()=='') {
+            company.addClass('error');
             returnError = true;
-        } else message.removeClass('error');
+        } else company.removeClass('error');
 
         // Highlight all error fields, then quit.
         if(returnError == true) {
@@ -34,7 +35,7 @@ $(function(){
         }
 
         //organize the data
-        var data = 'author=' + author.val() + '&phone=' + phone.val() + '&comment='  + encodeURIComponent(phone.val() + '\n' + message.val());
+        var data = 'author=' + author.val() + '&phone=' + phone.val() + '&comment='  + encodeURIComponent(phone.val() + '\n' + company.val() + '\n' + message.val());
         	data += '&comment_post_ID=' + comment_post_ID.val() + '&comment_parent=' + comment_parent.val();
         //disabled all the text fields
         // $('.text').attr('disabled','true');
@@ -46,25 +47,21 @@ $(function(){
             //this is the php file that processes the data and sends email
             // url: "inc/process.php",	
             url: template_url + '/comments-ajax.php',
-
             //GET method is used
             type: "POST",
-
             //pass the data	
             data: data,	
-
             //Do not cache the page
             cache: false,
-
             //success
             success: function (html) {
-                alert('您的信息我们已经收到，请耐心等待我们的回复');
+                alert('您的信息我们已经收到，请等待我们的回复');
                 author.val('');
 				phone.val('');
-				message.val('');	
+                company.val('');
+				message.val('');
             }	
         });
-
         return false;
 	});
 });
--- template_solution.php
+++ template_solution.php
@@ -7,29 +7,31 @@ get_header(); ?>
   <div class="mo-solution">
     <h3>免费索取策划方案</h3>
     <div class="recorde">最新申请<span>108</span>人 累计已有<span>1588</span>位索取方案</div>
-    <form>
+    <form method="post" action="#" onsubmit="return false;">
       <div class="form">
           <ul>
             <li class="require">
               <label for="name">姓 名</label>
-              <input type="text" class="text" id="name" />
+              <input type="text" name="author" class="text" id="name" />
             </li>
             <li class="require">
               <label for="phone">手 机</label>
-              <input type="num" class="text" id="phone" />
+              <input type="num" name="phone" class="text" id="phone" />
             </li>
             <li class="require">
               <label for="company">公 司</label>
-              <input type="text" class="text" id="company" />
+              <input type="text" name="company" class="text" id="company" />
             </li>
             <li>
               <label>备 注</label>
-              <textarea></textarea>
+              <textarea name="message"></textarea>
             </li>
           </ul>
       </div>
       <div class="btns">
-        <input type="button" class="submit" value="提交" />
+        <input type="hidden" name="comment_post_ID" value="1">
+        <input type="hidden" name="comment_parent" value="0">
+        <input type="button" class="submit" id="submit" value="提交" />
         <input type="reset" class="reset" value="重置" />
       </div>
     </form>
