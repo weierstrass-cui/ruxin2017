5dfb1fac2f75d75e5e7cb6e891088c992bd67fd0
weierstrass.cui <244169731@qq.com>
2017-02-18 13:16:25 +0800

    首页申请人数滚动

--- template_gallery.php
+++ template_gallery.php
@@ -112,7 +112,7 @@ get_header(); ?>
                     </div>
                     <div class="solutionList">
                         <div class="recorde">最新申请<span><?php echo get_post_meta('1','最新人数',true); ?></span>人 累计已有<span><?php echo get_post_meta('1','累计人数',true); ?></span>位索取方案</div>
-                        <div class="pc-solutionSlider">
+                        <div class="pc-solutionSlider" style="height: 156px;overflow: hidden;">
                           <ul>
                               <li>郭** 133****8787 5分钟前</li>
                               <li>宋** 135****7982 6分钟前</li>
