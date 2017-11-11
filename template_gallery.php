41f86f0630ee8eb87f66416079b28ae74ad8e13a
weierstrass.cui <244169731@qq.com>
2017-11-11 14:16:28 +0800

    ##

--- template_gallery.php
+++ template_gallery.php
@@ -81,10 +81,12 @@ get_header(); ?>
                 <div class="swiper-button-next material-icons-chevron_right"></div>
             </div>
             <div id="thePost" class="thePost">
-                <div style="width:100%;font-size:1.4rem;margin:0 auto 2rem;line-height:1.5rem;">
-                    <div>策划/设计师：</div>
-                    <div style="text-indent:3rem;"><?php echo get_post_meta($postID,'理念',true); ?></div>
-                </div>
+                <?php if( get_post_meta($postID,'理念',true) ){ ?>
+                    <div style="width:100%;font-size:1.4rem;margin:0 auto 2rem;line-height:2rem;font-weight:bold;">
+                        <div>策划/设计师：</div>
+                        <div style="text-indent:3rem;"><?php echo get_post_meta($postID,'理念',true); ?></div>
+                    </div>
+                <?php } ?>
                 <?php echo $this_post->post_content; ?>
             </div>
             <div id="theImages"></div>
