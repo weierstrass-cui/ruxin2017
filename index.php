6ce6cfeb78fab14db771e69215660b5560355566
weierstrass.cui <244169731@qq.com>
2017-11-16 21:31:38 +0800

    ###

--- index.php
+++ index.php
@@ -132,7 +132,9 @@ get_header(); ?>
         </div>
         <script src="<?php bloginfo('template_url'); ?>/js/jquery.mousewheel.min.js"></script>
         <script src="<?php bloginfo('template_url'); ?>/js/jquery.carousel-1.1.min.js"></script>
-        <script>$(function(){$('.carousel').carousel({carouselWidth:930,carouselHeight:330,directionNav:true,shadow:true,buttonNav:'bullets'});});</script>
+        <script>$(function(){
+          debugger;
+          $('.carousel').carousel({carouselWidth:930,carouselHeight:330,directionNav:true,shadow:true,buttonNav:'bullets'});});</script>
         <!-- END Owl Carousel -->
       </div>
     </section>
