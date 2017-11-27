24f24574544a6efba8ec824e6215146d42d2b8cc
崔楹彪 <244169731@qq.com>
2017-11-27 15:27:24 +0800

    ####

--- template_message.php
+++ template_message.php
@@ -3,7 +3,7 @@
 Template Name:留言
 */
 ?>
-<?php global $wpdb; $query = "SELECT * as count FROM ".$wpdb->prefix."comments where comment_post_ID = 1 order by comment_date desc;";
+<?php global $wpdb; $query = "SELECT * FROM ".$wpdb->prefix."comments where comment_post_ID = 1 order by comment_date desc;";
       $firstrow = $wpdb->get_results($query);
   ?>
   <ul>
