d0a95e4647b63233e60e474c473d17238cdc4d23
崔楹彪 <244169731@qq.com>
2017-11-27 15:35:40 +0800

    ###

--- template_message.php
+++ template_message.php
@@ -6,13 +6,13 @@ Template Name:留言
 <?php global $wpdb; $query = "SELECT * FROM ".$wpdb->prefix."comments where comment_post_ID = 1 order by comment_date desc;";
       $firstrow = $wpdb->get_results($query);
   ?>
-  <table>
+  <table border="1">
   <?php  foreach($firstrow as $v){ ?>
     <tr>
-      <td>
+      <td style="width:25%;">
         <?php echo $v->comment_date; ?>
       </td>
-      <td>
+      <td style="width:75%;">
       <?php echo $v->comment_content; ?>
       </td>
     </tr>
